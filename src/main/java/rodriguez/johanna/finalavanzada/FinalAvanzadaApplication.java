7527dda2cb83f089cad13c7a6e36c870184e234b
Johanna Rodriguez <39653403+Johannarr@users.noreply.github.com>
2019-12-30 14:06:29 -0400

    Creando proj

---
+++ src/main/java/rodriguez/johanna/finalavanzada/FinalAvanzadaApplication.java
@@ -0,0 +1,13 @@
+package rodriguez.johanna.finalavanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAvanzadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalAvanzadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rodriguez/johanna/finalavanzada/FinalAvanzadaApplicationTests.java
@@ -0,0 +1,13 @@
+package rodriguez.johanna.finalavanzada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAvanzadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
